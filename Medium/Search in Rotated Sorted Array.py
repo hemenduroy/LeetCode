6d45902498a3018e167221c439daef1fa9fa4aff
Hemendu Roy <89282414+hemenduroy@users.noreply.github.com>
2022-03-10 14:57:36 -0800

    Update Search in Rotated Sorted Array.py

--- Medium/Search in Rotated Sorted Array.py
+++ Medium/Search in Rotated Sorted Array.py
@@ -29,3 +29,49 @@ class Solution:
         return -1
    '''
    #Binary Search
+       def search(self, nums: List[int], target: int) -> int:
+        length=len(nums)
+
+        mid=length//2
+        #length is 1
+        if length==1:
+            if nums[0]==target:
+                return 0
+            else:
+                return -1
+
+        #mid is target
+        if nums[mid]==target:
+            return mid
+        #finding if we're to the left or right of the inflection point
+        #array is not pivoted
+        if nums[length-1]>nums[0]:
+            start=0
+            end=length-1
+        else:
+            if nums[mid]>=nums[0]:
+                if target>=nums[0]:
+                    start=0
+                    end=mid-1
+                else:
+                    start=mid+1
+                    end=length-1
+            else:
+                if target<nums[length-1] and target<nums[mid]:
+                    start=0
+                    end=mid-1
+                else:
+                    start=mid+1
+                    end=length-1
+
+        print(start,end)
+        #binary search starts
+        while start<=end:
+            mid=(start+end)//2
+            if nums[mid]==target:
+                return mid
+            if target>nums[mid]:
+                start=mid+1
+            else:
+                end=mid-1
+        return -1
