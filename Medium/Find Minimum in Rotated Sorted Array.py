52b7af9033d3518a3a6f4d6b4b9bb60495f9de50
Hemendu Roy <89282414+hemenduroy@users.noreply.github.com>
2022-03-10 12:39:34 -0800

    Update Find Minimum in Rotated Sorted Array.py

--- Medium/Find Minimum in Rotated Sorted Array.py
+++ Medium/Find Minimum in Rotated Sorted Array.py
@@ -4,6 +4,8 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
       return min(nums)
     '''
+    #Linear Search
+    '''
     def findMin(self, nums: List[int]) -> int:
         length=len(nums)
         for i in range(length):
@@ -13,3 +15,23 @@ class Solution:
                 continue
             else:
                 return nums[i+1]
+     '''
+     #Binary Search
+     def findMin(self, nums: List[int]) -> int:
+        length=len(nums)
+        if length==1:
+            return nums[0]
+        if nums[length-1]>nums[0]:
+            return nums[0]
+        start=0
+        end=length-1
+        while True:
+            mid=(end+start)//2
+            if nums[mid] > nums[mid+1]:
+                return nums[mid+1]
+            if nums[mid-1] > nums[mid]:
+                return nums[mid]
+            if nums[mid] > nums[start]:
+                start=mid+1
+            else:
+                end=mid-1
