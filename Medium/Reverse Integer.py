da324ecf15512629c916ede17139f78cf2a35dc7
Hemendu Roy <89282414+hemenduroy@users.noreply.github.com>
2022-03-10 20:36:33 -0700

    Create Reverse Integer.py

---
+++ Medium/Reverse Integer.py
@@ -0,0 +1,18 @@
+class Solution:
+  #Using str()
+  '''
+    def reverse(self, x: int) -> int:
+        k=1
+        if x<0:
+            x*=-1
+            k=0
+        
+        y=int(str(x)[::-1])
+        if k==0:
+            y*=-1
+        if y>2147483647 or y<-2147483647:
+            return 0
+        else:
+            return y
+  '''
+            
