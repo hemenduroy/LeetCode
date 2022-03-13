874203be2029dd0e65d48f4d6931ccd93f28c7bb
Hemendu Roy <89282414+hemenduroy@users.noreply.github.com>
2022-03-12 16:29:33 -0800

    Update Sort the Matrix Diagonally.py

--- Medium/Sort the Matrix Diagonally.py
+++ Medium/Sort the Matrix Diagonally.py
@@ -2,9 +2,11 @@ class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
         height=len(mat)
         width=len(mat[0])
+        #print(height,width)
         res=[]
-        res2=[[0]*width*height]
-        #print(res)
+        #res2=[[0]*width]*height
+        res2 = [ [0]*width for i in range(height)]
+        #print(res2)
         #print(height,width)
         i=height-1
         j=0
@@ -20,7 +22,8 @@ class Solution:
             i+=1
             j+=1
             if i==height or j==width:
-                res.append(sorted(tmp))
+                for x in sorted(tmp):
+                    res.append(x)
                 tmp=[]
                 if i==j:
                     mode=False
@@ -39,21 +42,23 @@ class Solution:
                 traversed=0
             #print(count)
             count-=1
-        print(res)
+        #print(res)
         #filling elements from res to res2
+        mode=True
         i=height-1
         j=0
         count=height*width
+        k=0
+        #print(len(res))
+        #print(res)
         while count>0:
-            
-            res2[i][j]=
+            #print(res2)
+            #print("res2["+str(i)+"]["+str(j)+"]="+str(res[k]))
+            res2[i][j]=res[k]
             traversed+=1
-            #print(i,j)
             i+=1
             j+=1
             if i==height or j==width:
-                res.append(sorted(tmp))
-                tmp=[]
                 if i==j:
                     mode=False
                 if mode:
@@ -71,5 +76,6 @@ class Solution:
                 traversed=0
             #print(count)
             count-=1
-        
-    
+            k+=1
+        #print("end of while")
+        return res2
