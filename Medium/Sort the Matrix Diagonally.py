a78c8da87e031cf67c2d07308f85f3f6461ba63d
Hemendu Roy <89282414+hemenduroy@users.noreply.github.com>
2022-03-13 22:58:07 -0700

    Update Sort the Matrix Diagonally.py

--- Medium/Sort the Matrix Diagonally.py
+++ Medium/Sort the Matrix Diagonally.py
@@ -82,14 +82,15 @@ class Solution:
         return res2
     '''
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
-    M,N=len(mat),len(mat[0])
-    new_mat=defaultdict(list)
-
-    for row in range(M):
-        for col in range(N):
-            heappush(new_mat[row-col],mat[row][col])
-
-    for row in range(M):
-        for col in range(N):
-            mat[row][col]=heappop(new_mat[row-col])
-    return mat
+        #row-col is a unique key for every diagonal
+        M,N=len(mat),len(mat[0])
+        new_mat=defaultdict(list)
+    
+        for row in range(M):
+            for col in range(N):
+                heappush(new_mat[row-col],mat[row][col])
+    
+        for row in range(M):
+            for col in range(N):
+                mat[row][col]=heappop(new_mat[row-col]) #heaps pop smallest first
+        return mat
