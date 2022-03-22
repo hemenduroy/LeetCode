3d96b631e0215f122d277e2f841f11bdbd781b6d
Hemendu Roy <89282414+hemenduroy@users.noreply.github.com>
2022-03-21 17:48:17 -0700

    Create Search Insert Position.java

---
+++ Easy/Search Insert Position.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        /*O(n)
+        int i=0
+        for(i=0;i<nums.length;i++){
+            if ((nums[i]==target) || (nums[i]>target)){
+                break;
+            }
+        }
+        return i;
+        */
+        //binary search 0(logn)
+        int start=0,end=nums.length-1;
+        int mid=0;
+        while(start<=end){
+            mid=(start+end)/2;
+            if (nums[mid]==target){
+                return mid;
+            }
+            else if (nums[mid]>target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }//end while
+        if(end<mid){
+                if(end==-1){
+                    return 0;
+                }
+                else{
+                    return mid;
+                }
+            }
+            else{
+                return start;
+            }
+    }
+}
