a0069d567f4fb14766c0feddf3ae5f8f0299cfb9
Hemendu Roy <89282414+hemenduroy@users.noreply.github.com>
2022-03-26 21:35:09 -0700

    Update Plus One.py

--- Easy/Plus One.py
+++ Easy/Plus One.py
@@ -14,3 +14,16 @@ class Solution:
             return [1]+digits
         else:
             return digits
+        
+    '''    
+    def plusOne(self, digits):
+        length = len(digits) - 1
+        while digits[length] == 9:
+            digits[length] = 0
+            length -= 1
+        if(length < 0):
+            digits = [1] + digits
+        else:
+            digits[length] += 1
+        return digits
+    '''
