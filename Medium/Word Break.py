cbe61139e24d9342849d4bbef813845a4b6587d7
Hemendu Roy <89282414+hemenduroy@users.noreply.github.com>
2022-03-28 23:30:45 -0700

    Create Word Break.py

---
+++ Medium/Word Break.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        length=len(s)
+        
+        dp=[False] * (length+1)
+        dp[length] = True
+        
+        for i in range(length-1,-1,-1):
+            for w in wordDict:
+                if i+len(w)<=length and s[i:i+len(w)]==w:
+                    dp[i]=dp[i+len(w)]
+                    if dp[i]:
+                        break
+        return dp[0]
