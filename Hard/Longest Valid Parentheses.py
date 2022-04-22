e29e5eded4715009c804cd068531c0494b84d150
Hemendu Roy <89282414+hemenduroy@users.noreply.github.com>
2022-04-21 19:00:12 -0700

    Update Longest Valid Parentheses.py

--- Hard/Longest Valid Parentheses.py
+++ Hard/Longest Valid Parentheses.py
@@ -1,4 +1,5 @@
 class Solution:
+    # time - O(n), space - O(n)
     def longestValidParentheses(self, s: str) -> int:
 
         stack = [-1]
@@ -17,3 +18,42 @@ class Solution:
                     res = max(res, i - stack[-1])
                 
         return res
+
+'''
+class Solution:
+    # time - O(n), space - O(1)
+    def longestValidParentheses(self, s: str) -> int:
+
+        open,close=0,0
+        
+        length=len(s)
+        res=0
+        
+        for i in range(length):
+            if s[i]=='(':
+                open+=1
+            else:
+                close+=1
+                
+            if open==close:
+                res=max(res,open+close)
+            if close>open:
+                #reset
+                open,close=0,0
+              
+        open,close=0,0
+        for i in range(length-1,-1,-1):
+            if s[i]=='(':
+                open+=1
+            else:
+                close+=1
+                
+            if open==close:
+                res=max(res,open+close)
+            if close<open:
+                #reset
+                open,close=0,0
+                
+        return res
+                
+'''
