45bc90b6dc78d43063fa33ea26c12d200e0e5070
Hemendu Roy <89282414+hemenduroy@users.noreply.github.com>
2022-04-29 10:40:15 -0700

    Update Serialize and Deserialize Binary Tree.py

--- Hard/Serialize and Deserialize Binary Tree.py
+++ Hard/Serialize and Deserialize Binary Tree.py
@@ -4,7 +4,7 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+# DFS
 class Codec:
     def serialize(self, root):
         def dfs(root, res):
@@ -46,3 +46,43 @@ class Codec:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
+
+# BFS
+class Codec:
+    def serialize(self, root):
+        if not root:
+            return []
+        lst, curr_level = [], [root]
+        while curr_level:
+            next_level, has_value = [], False
+            for node in curr_level:
+                if node:
+                    lst.append(node.val)
+                    next_level.append(node.left)
+                    next_level.append(node.right)
+                    if node.left or node.right:
+                        has_value = True
+                else:
+                    lst.append(None)
+            curr_level = next_level if has_value else []
+        return lst
+
+    def deserialize(self, data):
+        n = len(data)
+        if n == 0:
+            return None
+        root = TreeNode(data[0])
+        curr_level, i = [root], 1
+        while curr_level and i < n:
+            next_level = []
+            for node in curr_level:
+                if data[i] != None:
+                    node.left = TreeNode(data[i])
+                    next_level.append(node.left)
+                i += 1
+                if data[i] != None:
+                    node.right = TreeNode(data[i])
+                    next_level.append(node.right)
+                i += 1
+            curr_level = next_level
+        return root
